aec34d0f8133fc13e8fc40aa643abc89e2a1c558
hakvdev <matiayalapizarro@gmail.com>
2024-10-21 21:22:58 -0300

    Segundo Commit maestro

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { SetStateAction, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Texto() {
+  const [texto, setTexto] = useState("");
+  const leerTexto = (event: { target: { value: SetStateAction<string> } }) => {
+    setTexto(event.target.value);
+  };
+
+  const limpiar = () => {
+    setTexto("");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type="text" value={texto} onChange={leerTexto} />
+      <p>Aquí estará tu texto: {texto}</p>
+      <button onClick={limpiar}>Limpiar</button>
     </>
-  )
+  );
 }
 
-export default App
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const clickeo = () => {
+    setCount(count + 1);
+  };
+
+  const reiniciar = () => {
+    setCount(0);
+  };
+
+  return (
+    <>
+      <br></br>
+      <button onClick={clickeo}>Hiciste click {count} veces</button>
+      <button onClick={reiniciar}>Reiniciar</button>
+      <br></br>
+    </>
+  );
+}
+
+function MyButton() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
+  return (
+    <>
+      <button onClick={handleClick}>
+        {isActive ? "Estoy activo" : "Me desactivaste!"}
+      </button>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <h1>Bienvenido a mi aplicación</h1>
+      <MyButton />
+      <Counter />
+      <Texto />
+    </>
+  );
+}
+
+export default App;
